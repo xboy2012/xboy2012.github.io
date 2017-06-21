e8a64e94507d85e377ef4cb1555fc4d0524b3eee
xboy2012 <xboy2008@live.cn>
2017-06-21 20:12:17 +0800

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
 console.log('Hello, github');
+
+
+function removeIOSRubberEffect( element ) {
+
+    element.addEventListener( "touchstart", function () {
+
+        var top = element.scrollTop, totalScroll = element.scrollHeight, currentScroll = top + element.offsetHeight;
+
+        if ( top === 0 ) {
+            element.scrollTop = 1;
+        } else if ( currentScroll === totalScroll ) {
+            element.scrollTop = top - 1;
+        }
+
+    } );
+
+}
+
+
+
+removeIOSRubberEffect(document.querySelector('.main'));
