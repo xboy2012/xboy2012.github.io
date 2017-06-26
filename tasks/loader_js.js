b65de8514646ab0d40f6445ae57ad8e3bcc66071
xboy2012 <xboy2008@live.cn>
2017-06-26 20:19:41 +0800

    loader.js去除严格模式

--- tasks/loader_js.js
+++ tasks/loader_js.js
@@ -6,10 +6,12 @@ gulp.task('loader_js', () => {
     return rollup({
         entry: `${ROOT_DIR}/src/js/loader.js`,
         //plugins: [],
+
     })
     .then((bundle) => {
         bundle.write({
             format: "iife",
+            useStrict: false,   //去除严格模式，减少无用字符，同时增加代码兼容性
             dest: `${ROOT_DIR}/tmp/loader.js`
         });
     });
