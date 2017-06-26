c4b98c5f0e9ef9b548b00ae8a7ebfdf3fe0f71a1
xboy2012 <xboy2008@live.cn>
2017-06-26 16:18:20 +0800

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
 console.log('Hello, github');
 
-var el = document.querySelector('.main');
-
-
-const reset = () => {
-    el.scrollLeft = 400;
-    el.scrollTop = 400;
-};
-
-reset();
-el.addEventListener('touchstart', reset);
-
 import isIOS from './utils/isIOS';
 import {add} from './bouncefix/bouncefix';
 
