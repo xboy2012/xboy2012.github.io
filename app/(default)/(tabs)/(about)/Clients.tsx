9637de9c3df508530accb0b262a593279a8d6995
XBOY <xboy2008@live.cn>
2024-04-06 00:48:08 -0700

    feat: update-companies

--- app/(default)/(tabs)/(about)/Clients.tsx
+++ app/(default)/(tabs)/(about)/Clients.tsx
@@ -4,39 +4,78 @@ export const Clients = () => {
       <h3 className="h3 clients-title">Clients</h3>
 
       <ul className="clients-list has-scrollbar">
-        <li className="clients-item">
-          <a href="#">
-            <img src="/assets/logo-1-color.png" alt="client logo" />
+        <li className="clients-item overflow-hidden rounded">
+          <a href="https://www.microsoft.com" target="_blank">
+            <div
+              className="bg-center bg-no-repeat bg-cover mx-3 h-full"
+              style={{ backgroundImage: 'url("/assets/logo-microsoft.svg")' }}
+            />
           </a>
         </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="/assets/logo-2-color.png" alt="client logo" />
+        <li className="clients-item overflow-hidden rounded">
+          <a href="https://www.tencent.com" target="_blank">
+            <div
+              className="bg-center bg-no-repeat bg-contain mx-3 h-full"
+              style={{ backgroundImage: 'url("/assets/logo-tencent.svg")' }}
+            />
           </a>
         </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="/assets/logo-3-color.png" alt="client logo" />
+        <li className="clients-item overflow-hidden rounded">
+          <a href="https://www.nio.com" target="_blank">
+            <div
+              className="bg-center bg-no-repeat bg-contain h-full"
+              style={{ backgroundImage: 'url("/assets/logo-nio.svg")' }}
+            />
           </a>
         </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="/assets/logo-4-color.png" alt="client logo" />
+        <li className="clients-item overflow-hidden rounded">
+          <a href="https://leetcode.com" target="_blank">
+            <div
+              className="bg-center bg-no-repeat bg-contain mx-3 h-full"
+              style={{ backgroundImage: 'url("/assets/logo-leetcode.svg")' }}
+            />
           </a>
         </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="/assets/logo-5-color.png" alt="client logo" />
+        <li className="clients-item overflow-hidden rounded">
+          <a href="https://www.temu.com" target="_blank">
+            <div
+              className="bg-center bg-no-repeat bg-cover h-full"
+              style={{ backgroundImage: 'url("/assets/logo-temu.jpeg")' }}
+            />
           </a>
         </li>
 
-        <li className="clients-item">
-          <a href="#">
-            <img src="/assets/logo-6-color.png" alt="client logo" />
+        <li className="clients-item overflow-hidden rounded">
+          <a href="https://m.pinduoduo.com" target="_blank">
+            <div
+              className="bg-center bg-no-repeat bg-cover h-full"
+              style={{ backgroundImage: 'url("/assets/logo-pdd.jpeg")' }}
+            />
+          </a>
+        </li>
+
+        <li className="clients-item overflow-hidden rounded">
+          <a href="https://myshell.ai" target="_blank">
+            <div
+              className="bg-center bg-cover bg-no-repeat h-full"
+              style={{ backgroundImage: 'url("/assets/logo-myshell.jpeg")' }}
+            />
+          </a>
+        </li>
+
+        <li
+          className="clients-item overflow-hidden rounded"
+          style={{ background: "black" }}
+        >
+          <a href="https://www.wacai.com" target="_blank">
+            <div
+              className="bg-center bg-contain bg-no-repeat mx-3 h-full"
+              style={{ backgroundImage: 'url("/assets/logo-wacai.png")' }}
+            />
           </a>
         </li>
       </ul>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,65 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '450px',
+      'md': '580px',
+      'lg': '768px',
+      'xl': '1024px',
+      '2xl': '1250px',
+    },
+    fontSize: {
+      '1': '24px',
+      '2': '18px',
+      '3': '17px',
+      '4': '16px',
+      '5': '15px',
+      '6': '14px',
+      '7': '13px',
+      '8': '11px',
+    },
+    fontWeight: {
+      '200': '300',
+      '400': '400',
+      '500': '500',
+      '600': '600',
+    },
+    colors: {
+      'jet': 'hsl(0, 0%, 22%)',
+      'onyx': 'hsl(240, 1%, 17%)',
+      'eerieBlack1': 'hsl(240, 2%, 13%)',
+      'eerieBlack2': 'hsl(240, 2%, 12%)',
+      'smokyBlack': 'hsl(0, 0%, 7%)',
+      'white1': 'hsl(0, 0%, 100%)',
+      'white2': 'hsl(0, 0%, 98%)',
+      'orangeYellowCrayola': 'hsl(45, 100%, 72%)',
+      'vegasGold': 'hsl(45, 54%, 58%)',
+      'lightGray': 'hsl(0, 0%, 84%)',
+      'lightGray70': 'hsla(0, 0%, 84%, 0.7)',
+      'bittersweetShimmer': 'hsl(0, 43%, 51%)',
+
+      'navbarBg': 'hsla(240, 1%, 17%, 0.75)',
+    },
     extend: {
+      spacing: {
+        '7.5': '30px'
+      },
+      borderRadius: {
+        '2.5xl': '20px'
+      },
+      boxShadow: {
+        '1': '-4px 8px 24px hsla(0, 0%, 0%, 0.25)',
+        '1xl': '-4px 8px 24px hsla(0, 0%, 0%, 0.125)',
+        '2': '0 16px 30px hsla(0, 0%, 0%, 0.25)',
+        '2xl': '0 16px 30px hsla(0, 0%, 0%, 0.125)',
+        '3': '0 16px 40px hsla(0, 0%, 0%, 0.25)',
+        '3xl': '0 16px 40px hsla(0, 0%, 0%, 0.125)',
+        '4': '0 25px 50px hsla(0, 0%, 0%, 0.15)',
+        '5': '0 24px 80px hsla(0, 0%, 0%, 0.25)',
+      },
+      zIndex: {
+        '5': '5',
+      }
       // backgroundImage: {
       //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       //   "gradient-conic":
