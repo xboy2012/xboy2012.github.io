06123bbbd6457c3995f374129d9ea0b048f3c157
XBOY <xboy2008@live.cn>
2024-04-10 18:57:38 -0700

    chore: refactor styles

--- components/SideBar/InfoCard.tsx
+++ components/SideBar/InfoCard.tsx
@@ -1,7 +1,7 @@
+import cx from 'classnames';
 import { AvatarBox } from './AvatarBox';
 import { MoreButton } from './MoreButton';
 import { userData } from '../../src/data';
-import styles from './InfoCard.module.css';
 
 interface Props {
   onMoreClick?: () => void;
@@ -10,13 +10,26 @@ interface Props {
 export const InfoCard = ({ onMoreClick }: Props) => {
   const { avatar, name, title } = userData;
   return (
-    <div className={styles.sideBarInfo}>
+    <div className="relative flex justify-start items-center gap-[15px] md:gap-[25px] 2xl:flex-col">
       <AvatarBox avatar={avatar} alt={name} />
       <div>
-        <h1 className={styles.name} title={name}>
+        <h1
+          className={cx(
+            'text-white2 text-3 font-500 -tracking-[0.25px] mb-2.5',
+            'md:mb-[15px] 2xl:whitespace-nowrap 2xl:text-center',
+          )}
+          title={name}
+        >
           {name}
         </h1>
-        <p className={styles.title}>{title}</p>
+        <p
+          className={cx(
+            'text-white1 bg-onyx text-8 font-300 w-max py-[3px] px-3 rounded-lg',
+            'md:py-[5px] md:px-[18px] 2xl:m-auto',
+          )}
+        >
+          {title}
+        </p>
       </div>
       <MoreButton onClick={onMoreClick} />
     </div>
