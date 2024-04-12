9b06d1395a235eaa0406afc68740e02358d511f1
XBOY <xboy2008@live.cn>
2024-04-12 16:55:45 -0700

    chore: add lintstaged

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,18 @@
+import micromatch from 'micromatch';
+
+/** @type {import('lint-staged').Config} */
+const config = {
+  '*.{ts,tsx,mts,cts}': () => 'tsc --noEmit',
+  '*.{js,jsx,cjs,mjs,ts,tsx,mts,cts}': ['prettier --write', 'eslint --fix'],
+  '*': (files) => {
+    const otherFiles = micromatch.not(files, [
+      '**/*.{js,jsx,cjs,mjs,ts,tsx,mts,cts}',
+    ]);
+    if (otherFiles.length) {
+      return `prettier --write --ignore-unknown '${otherFiles.join("' '")}'`;
+    }
+    return null;
+  },
+};
+
+export default config;
