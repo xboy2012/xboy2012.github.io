295e2608029f613c2d87e16e48c597e9986c18d9
XBOY <xboy2008@live.cn>
2024-05-05 11:55:35 -0700

    chore: reuse userData for meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
+import { userData } from '../src/data';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Nex Liu',
-  description:
-    'I am a seasoned software engineer, with a solid foundation in Computer Science & Technology. My expertise, honed through roles at many well-known companies, such as NIO, Microsoft, LeetCode, Temu, and Tencent, encompasses a diverse tech stack. Notable achievements include refactoring legacy codebases, leading internationalization efforts, and optimizing web performance. I am eager to bring my skills and commitment to excellence to contribute effectively to challenging projects.',
+  title: userData.name,
+  description: userData.intro.join(''),
 };
 
 export default function RootLayout({
--- app/manifest.ts
+++ app/manifest.ts
@@ -1,12 +1,12 @@
 import type { MetadataRoute } from 'next';
 import { colors } from '../src/config/colors';
+import { userData } from '../src/data';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Next Liu's Personal Website",
-    short_name: 'Nex Liu',
-    description:
-      'I am a seasoned software engineer, with a solid foundation in Computer Science & Technology. My expertise, honed through roles at many well-known companies, such as NIO, Microsoft, LeetCode, Temu, and Tencent, encompasses a diverse tech stack. Notable achievements include refactoring legacy codebases, leading internationalization efforts, and optimizing web performance. I am eager to bring my skills and commitment to excellence to contribute effectively to challenging projects.',
+    name: userData.name,
+    short_name: userData.name,
+    description: userData.intro.join(''),
     start_url: '/',
     display: 'standalone',
     background_color: colors.smokyBlack,
