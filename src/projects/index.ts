0e4f7a3c21e7321046177a1ac9a8d110750112c5
XBOY <xboy2008@live.cn>
2024-05-06 00:42:22 -0700

    chore: make project images static build

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,4 +1,9 @@
-import type { ProjectData } from './types';
+import type { ProjectData } from '../types';
+
+const src = (obj: any): string => {
+  const defaultValue = (obj && obj.default) || obj;
+  return defaultValue.src;
+};
 
 export const projects: ProjectData[] = [
   {
@@ -6,62 +11,62 @@ export const projects: ProjectData[] = [
     category: 'Web development',
     link: '#',
     title: 'Finance',
-    image: '/assets/project-1.jpg',
+    image: src(require('./images/project-1.jpg')),
   },
   {
     id: 2,
     category: 'Web design',
     link: '#',
     title: 'ABC',
-    image: '/assets/project-2.png',
+    image: src(require('./images/project-2.png')),
   },
   {
     id: 3,
     category: 'Web design',
     link: '#',
     title: 'Fundo',
-    image: '/assets/project-3.jpg',
+    image: src(require('./images/project-3.jpg')),
   },
   {
     id: 4,
     category: 'Applications',
     link: '#',
     title: 'Brawlhalla',
-    image: '/assets/project-4.png',
+    image: src(require('./images/project-4.png')),
   },
   {
     id: 5,
     category: 'Web design',
     link: '#',
     title: 'DSM.',
-    image: '/assets/project-5.png',
+    image: src(require('./images/project-5.png')),
   },
   {
     id: 6,
     category: 'Web design',
     link: '#',
     title: 'MetaSpark',
-    image: '/assets/project-6.png',
+    image: src(require('./images/project-6.png')),
   },
   {
     id: 7,
     category: 'Web development',
     link: '#',
     title: 'Summary',
-    image: '/assets/project-7.png',
+    image: src(require('./images/project-7.png')),
   },
   {
     id: 8,
     category: 'Applications',
     link: '#',
     title: 'Task Manager',
-    image: '/assets/project-8.jpg',
+    image: src(require('./images/project-8.jpg')),
   },
   {
     id: 9,
     category: 'Web development',
     link: '#',
     title: 'Arrival',
-    image: '/assets/project-9.png',
+    image: src(require('./images/project-9.png')),
   },
 ];
