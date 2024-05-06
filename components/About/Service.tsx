1d25eb3146f83f693ceb2c6c382bd51f9bad2778
XBOY <xboy2008@live.cn>
2024-05-06 00:15:41 -0700

    chore: make icons static build

--- components/About/Service.tsx
+++ components/About/Service.tsx
@@ -1,4 +1,8 @@
 import { ServiceItem } from './ServiceItem';
+import iconFrontend from './images/icon-frontend.svg';
+import iconFullstack from './images/icon-fullstack.svg';
+import iconArch from './images/icon-arch.svg';
+import iconDevOps from './images/icon-devops.svg';
 
 export const Service = () => {
   return (
@@ -10,22 +14,22 @@ export const Service = () => {
       <ul className="grid grid-cols-1fr gap-[20px] xl:grid-cols-1fr1fr xl:gap-y-[20px] xl:gap-x-[25px]">
         <ServiceItem
           name="Frontend Development"
-          icon="/assets/icon-frontend.svg"
+          icon={iconFrontend.src}
           desc="High-quality frontend development using various frameworks"
         />
         <ServiceItem
           name="Full Stack Development"
-          icon="/assets/icon-fullstack.svg"
+          icon={iconFullstack.src}
           desc="Isomorphic development for both frontend and backend"
         />
         <ServiceItem
           name="Software Architect"
-          icon="/assets/icon-arch.svg"
+          icon={iconArch.src}
           desc="Design comprehensive and effective system architect for commercial software"
         />
         <ServiceItem
           name="DevOps"
-          icon="/assets/icon-devops.svg"
+          icon={iconDevOps.src}
           desc="Leverage my expertise in CI/CD, improving efficiency and reducing errors"
         />
       </ul>
