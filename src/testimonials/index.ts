a761619da9a65cc9850701f1b583bec00968f415
XBOY <xboy2008@live.cn>
2024-05-06 01:16:30 -0700

    chore: make testimonials images static build

--- src/testimonials/index.ts
+++ src/testimonials/index.ts
@@ -1,28 +1,33 @@
-import type { TestimonialData } from './types';
+import type { TestimonialData } from '../types';
+
+const src = (obj: any): string => {
+  const defaultValue = (obj && obj.default) || obj;
+  return defaultValue.src;
+};
 
 // TODO: FAKE DATA
 export const testimonials: TestimonialData[] = [
   {
     name: 'Daniel lewis',
-    avatar: '/assets/avatar-1.png',
+    avatar: src(require('./images/avatar-1.png')),
     datetime: '2024-01-01',
     text: 'Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia.',
   },
   {
     name: 'Jessica miller',
-    avatar: '/assets/avatar-2.png',
+    avatar: src(require('./images/avatar-2.png')),
     datetime: '2024-01-02',
     text: 'Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia.',
   },
   {
     name: 'Emily evans',
-    avatar: '/assets/avatar-3.png',
+    avatar: src(require('./images/avatar-3.png')),
     datetime: '2024-04-03',
     text: 'Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia.',
   },
   {
     name: 'Henry william',
-    avatar: '/assets/avatar-4.png',
+    avatar: src(require('./images/avatar-4.png')),
     datetime: '2023-12-31',
     text: 'Richard was hired to create a corporate identity. We were very pleased with the work done. She has a lot of experience and is very concerned about the needs of client. Lorem ipsum dolor sit amet, ullamcous cididt consectetur adipiscing elit, seds do et eiusmod tempor incididunt ut laborels dolore magnarels alia.',
   },
