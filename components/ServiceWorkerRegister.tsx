160b89cd204ec7800a8c0400150633cb26d9ece4
XBOY <xboy2008@live.cn>
2024-05-11 14:38:02 -0700

    chore: open serviceWorker to all users

--- components/ServiceWorkerRegister.tsx
+++ components/ServiceWorkerRegister.tsx
@@ -1,21 +1,12 @@
 'use client';
 
 import { useEffect } from 'react';
-import { isExperimental } from '../src/experimental';
 
 export const ServiceWorkerRegister = () => {
   // install service worker to current page
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
       if (!('serviceWorker' in navigator)) {
-        return;
-      }
-      if (!isExperimental()) {
-        navigator.serviceWorker.getRegistrations().then((registrations) => {
-          for (const registration of registrations) {
-            registration.unregister();
-          }
-        });
         return;
       }
       (async () => {
