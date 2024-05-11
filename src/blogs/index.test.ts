2cbab045a882ac606baeced4b1a3e0abc90415d2
XBOY <xboy2008@live.cn>
2024-05-11 14:17:59 -0700

    chore: add test for distinct blogIds

--- src/blogs/index.test.ts
+++ src/blogs/index.test.ts
@@ -5,6 +5,20 @@ import { getBlogs } from './index';
 describe('blogs data should follow some conventions', () => {
   const rootDir = process.cwd();
 
+  test('blogs should have distinct ids', () => {
+    const set = new Set<string>();
+    const duplicatedIds: string[] = [];
+    for (const blog of getBlogs()) {
+      const blogId = blog.id;
+      if (set.has(blogId)) {
+        duplicatedIds.push(blogId);
+      } else {
+        set.add(blogId);
+      }
+    }
+    expect(duplicatedIds).toHaveLength(0);
+  });
+
   for (const blog of getBlogs()) {
     if (!blog.link) {
       const blogId = blog.id;
