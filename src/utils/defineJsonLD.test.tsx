be4fa78f83b24e1838252e1937afd74743645b31
XBOY <xboy2008@live.cn>
2024-05-24 01:28:58 -0700

    chore: improve defineJsonLD to support multiple JSON

--- src/utils/defineJsonLD.test.tsx
+++ src/utils/defineJsonLD.test.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import type { ReactElement } from 'react';
+import { type ReactElement, createElement } from 'react';
 import { useServerInsertedHTML } from 'next/navigation';
 import { defineJsonLD } from './defineJsonLD';
 
@@ -11,22 +11,38 @@ jest.mock('next/navigation', () => {
 });
 
 describe('defineJsonLD() test', () => {
-  test('should work as expected', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    let json: any;
+  for (let inputCount = 1; inputCount <= 6; inputCount++) {
+    test(`should work with ${inputCount} inputs`, () => {
+      let injectedScriptJSX: ReactElement;
 
-    jest.mocked(useServerInsertedHTML).mockImplementation((callback) => {
-      const element = callback() as ReactElement;
-      json = JSON.parse(element.props.dangerouslySetInnerHTML.__html);
+      jest.mocked(useServerInsertedHTML).mockImplementation((callback) => {
+        injectedScriptJSX = callback() as ReactElement;
+      });
+
+      const jsons = new Array(inputCount).fill(null).map((_, index) => {
+        return {
+          index,
+          value: Math.random().toString(36).substring(2),
+        };
+      });
+
+      const fns = jsons.map((json) => () => json);
+
+      // @ts-expect-error for testing purpose
+      const JsonLD = defineJsonLD(...fns);
+
+      const result = render(<JsonLD />);
+      // The <JsonLD> Component should return null, the script injection logic should be handled by Next.js
+      expect(result.container.innerHTML).toBeFalsy();
+
+      const expectHtml = jsons
+        .map((json) => {
+          return `<script type="application/ld+json">${JSON.stringify(json)}</script>`;
+        })
+        .join('');
+      const resultHtml = render(createElement(() => injectedScriptJSX))
+        .container.innerHTML;
+      expect(resultHtml).toBe(expectHtml);
     });
-
-    const mockGetData = jest.fn(() => ({ a: 1, b: 2 }));
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const JsonLD = defineJsonLD<any>(mockGetData);
-
-    render(<JsonLD />);
-
-    expect(json).toEqual({ a: 1, b: 2 });
-  });
+  }
 });
