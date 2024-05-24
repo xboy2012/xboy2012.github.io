43c26873d32444af6ffadd293f4c62639106c887
XBOY <xboy2008@live.cn>
2024-05-24 01:55:15 -0700

    chore: remove useless type annotation

--- components/About/JsonLD.ts
+++ components/About/JsonLD.ts
@@ -1,7 +1,6 @@
 'use client';
 
-import type { Person } from 'schema-dts';
 import { defineJsonLD } from '../../src/utils/defineJsonLD';
 import { getAboutJsonLD } from '../../src/json-ld/getAboutJsonLD';
 
-export const JsonLD = defineJsonLD<Person>(getAboutJsonLD);
+export const JsonLD = defineJsonLD(getAboutJsonLD);
--- components/Resume/JsonLD.ts
+++ components/Resume/JsonLD.ts
@@ -1,7 +1,6 @@
 'use client';
 
-import type { Person } from 'schema-dts';
 import { defineJsonLD } from '../../src/utils/defineJsonLD';
 import { getResumeJsonLD } from '../../src/json-ld/getResumeJsonLD';
 
-export const JsonLD = defineJsonLD<Person>(getResumeJsonLD);
+export const JsonLD = defineJsonLD(getResumeJsonLD);
