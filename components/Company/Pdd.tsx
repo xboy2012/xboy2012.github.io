a4d67266d85540574f3e5d7c85825ec02970b211
XBOY <xboy2008@live.cn>
2024-06-05 10:18:09 -0700

    chore: update Pdd image

--- components/Company/Pdd.tsx
+++ components/Company/Pdd.tsx
@@ -1,5 +1,5 @@
 export const Pdd = () => {
   return (
-    <div className="bg-center bg-no-repeat bg-cover h-full rounded overflow-hidden bg-pdd" />
+    <div className="h-full rounded bg-center bg-no-repeat bg-cover bg-pdd" />
   );
 };
