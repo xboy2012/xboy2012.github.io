280580b097cb7985579d5807fd544cd55c23d5c2
XBOY <xboy2008@live.cn>
2024-06-05 11:24:48 -0700

    chore: optimizing BingIndexing postbuild

--- scripts/bingIndexing/postbuild/index.ts
+++ scripts/bingIndexing/postbuild/index.ts
@@ -2,11 +2,18 @@ import { writeFile } from 'node:fs/promises';
 import { join as pathJoin } from 'node:path';
 
 (async () => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(
+      'Skipped generating Bing IndexNow key file in non-production environment',
+    );
+    return;
+  }
+
   // see https://www.bing.com/indexnow/getstarted#
 
   const key = process.env.BING_INDEX_NOW_KEY;
   if (!key) {
-    return;
+    throw Error('could not found BING_INDEX_NOW_KEY');
   }
 
   const rootDir = process.cwd();
