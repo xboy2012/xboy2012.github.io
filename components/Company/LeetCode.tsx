ddf848651319a485fffd94dc8d23a1a1d540fc0a
XBOY <xboy2008@live.cn>
2024-06-05 09:29:29 -0700

    chore: update LeetCode image

--- components/Company/LeetCode.tsx
+++ components/Company/LeetCode.tsx
@@ -1,7 +1,5 @@
 export const LeetCode = () => {
   return (
-    <div className="w-full h-full bg-white rounded">
-      <div className="bg-center bg-no-repeat bg-contain mx-3 h-full bg-leetcode" />
-    </div>
+    <div className="h-full rounded bg-center bg-no-repeat bg-contain bg-leetcode bg-white" />
   );
 };
