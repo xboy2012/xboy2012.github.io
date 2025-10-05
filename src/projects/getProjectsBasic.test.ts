b51e54595d52986f1ac600981b493702a7bc6653
XBOY <xboy2008@live.cn>
2025-10-05 13:21:15 -0700

    test: improve test case for getProjectsBasic

--- src/projects/getProjectsBasic.test.ts
+++ src/projects/getProjectsBasic.test.ts
@@ -1,6 +1,12 @@
 import { getProjectsBasic } from './getProjectsBasic';
 
 describe('projects data should follow some conventions', () => {
+  test('should return the same reference', () => {
+    const result1 = getProjectsBasic();
+    const result2 = getProjectsBasic();
+    expect(result1).toBe(result2);
+  });
+
   test('projects should have distinct ids', () => {
     const set = new Set<string>();
     const duplicatedIds: string[] = [];
