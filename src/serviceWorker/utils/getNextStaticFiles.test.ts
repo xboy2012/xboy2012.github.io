ce54768a473a0f65e6a1b2f85da34cc262c99ae8
XBOY <xboy2008@live.cn>
2025-10-05 13:15:55 -0700

    test: add test case for getNextStaticFiles

---
+++ src/serviceWorker/utils/getNextStaticFiles.test.ts
@@ -0,0 +1,14 @@
+import { getNextStaticFiles } from './getNextStaticFiles';
+
+describe('getNextStaticFiles', () => {
+  test('should return correctly', () => {
+    const NEXT_STATIC_FILES = ['/_next/static/js-01234567.js'];
+    // @ts-expect-error mock inject value
+    global.NEXT_STATIC_FILES = NEXT_STATIC_FILES;
+
+    const result1 = getNextStaticFiles();
+    expect(result1).toEqual(NEXT_STATIC_FILES);
+    const result2 = getNextStaticFiles();
+    expect(result2).toBe(result1);
+  });
+});
