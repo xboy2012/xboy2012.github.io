2d0c16692536a0474d68e9697d3b33465f029fea
XBOY <xboy2008@live.cn>
2025-10-07 15:12:37 -0700

    chore: opt variables

--- components/About/ClientItem.tsx
+++ components/About/ClientItem.tsx
@@ -37,7 +37,7 @@ export const ClientItem = ({ name, link }: { name: string; link: string }) => {
         />
         <p
           className={cx(
-            'absolute -left-[1000px] -top-[1000px]',
+            'absolute -left-1000 -top-1000',
             'print:relative print:left-auto print:top-auto print:font-300',
           )}
         >
