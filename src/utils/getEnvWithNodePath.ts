b07b86b1ac1b0c73eb916f719e97639bcd79fb40
XBOY <xboy2008@live.cn>
2025-11-10 13:12:27 -0800

    chore: opt getEnvWithNodePath

--- src/utils/getEnvWithNodePath.ts
+++ src/utils/getEnvWithNodePath.ts
@@ -1,14 +1,16 @@
 import { getRootDir } from './getRootDir';
-import { delimiter, join } from 'node:path';
+import { join } from 'node:path';
 import { getEnvPathKey } from './getEnvPathKey';
 import { getProcessEnv } from './getProcessEnv';
+import { prependEnvPath } from './prependEnvPath';
 
 export const getEnvWithNodePath = (): typeof process.env => {
   const rootDir = getRootDir();
   const binDir = join(rootDir, 'node_modules', '.bin');
   const pathKey = getEnvPathKey();
   const env = getProcessEnv();
-  const oldPath = env[pathKey];
-  const newPath = oldPath ? `${binDir}${delimiter}${oldPath}` : binDir;
-  return { ...env, [pathKey]: newPath };
+  return {
+    ...env,
+    [pathKey]: prependEnvPath(env[pathKey], binDir),
+  };
 };
