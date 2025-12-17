73974ba4d690ada40a3cc6457c9b6f6a6fd10564
XBOY <xboy2008@live.cn>
2025-12-16 17:12:18 -0800

    feat: serviceWork var -> const

--- tools/buildServiceWorker/bundleCode.ts
+++ tools/buildServiceWorker/bundleCode.ts
@@ -1,6 +1,7 @@
 import { build } from 'esbuild';
 import { minify } from 'terser';
 import { format } from 'prettier';
+import { ESLint } from 'eslint';
 
 export const bundleCode = async (options: {
   entry: string;
@@ -8,6 +9,8 @@ export const bundleCode = async (options: {
   minify: boolean;
 }): Promise<string> => {
   const shouldMinify = options.minify;
+
+  // bundle with esbuild
   const esbuildResult = await build({
     entryPoints: [options.entry],
     bundle: true,
@@ -21,7 +24,26 @@ export const bundleCode = async (options: {
   });
   const codeFromEsBuild = esbuildResult.outputFiles[0].text;
 
-  const terserOutput = await minify(codeFromEsBuild, {
+  // convert var to let/const
+  const eslint = new ESLint({
+    fix: true,
+    overrideConfigFile: true,
+    overrideConfig: {
+      languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      rules: {
+        'prefer-const': 'error',
+        'no-var': 'error',
+      },
+    },
+  });
+  const results = await eslint.lintText(codeFromEsBuild);
+  const codeFromEsLint = results[0]!.output!;
+
+  // trim dead code and minify with terser
+  const terserOutput = await minify(codeFromEsLint, {
     compress: {
       passes: 3,
       dead_code: true,
@@ -35,10 +57,12 @@ export const bundleCode = async (options: {
   });
   const codeFromTerser = terserOutput.code!;
 
+  // return minified code for production
   if (shouldMinify) {
     return codeFromTerser;
   }
 
+  // format with prettier
   const formatedCode = await format(codeFromEsBuild, { parser: 'babel' });
   return formatedCode;
 };
